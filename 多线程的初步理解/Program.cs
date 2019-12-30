d551465a4f8026423431fad958e596a9b9eb0d45
wangongshen@gmail.com <wangongshen@gmail.com>
2019-12-30 11:28:09 +0800

    添加多线程初步理解

---
+++ 多线程的初步理解/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 多线程的初步理解
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //通过new实例化方式
+            //Thread thread = new Thread(new ThreadStart(ThreadA));
+            //直接传入相应的无参无返回值方法——语法糖（允许这种简写方式，运行时编译器会自动加上）
+            Thread thread = new Thread(ThreadA);
+            //调用Start方法启动线程
+            thread.Start();
+            Console.ReadKey();
+        }
+
+        static void ThreadA() {
+            Console.WriteLine("这是个无参的方法");
+        }
+    }
+}
