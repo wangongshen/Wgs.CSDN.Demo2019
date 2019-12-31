ba9977bd010ef3c1b2baf8e708781baa2d4d76e1
wangongshen@gmail.com <wangongshen@gmail.com>
2019-12-31 18:16:56 +0800

    Socket通讯服务端【完成】

---
+++ SocketChatServer/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SocketChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.BtnStartListing = new System.Windows.Forms.Button();
+            this.cboUsers = new System.Windows.Forms.ComboBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnZD = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(167, 21);
+            this.txtIP.TabIndex = 0;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(207, 12);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(107, 21);
+            this.txtPort.TabIndex = 0;
+            // 
+            // BtnStartListing
+            // 
+            this.BtnStartListing.Location = new System.Drawing.Point(335, 12);
+            this.BtnStartListing.Name = "BtnStartListing";
+            this.BtnStartListing.Size = new System.Drawing.Size(108, 20);
+            this.BtnStartListing.TabIndex = 1;
+            this.BtnStartListing.Text = "开始监听";
+            this.BtnStartListing.UseVisualStyleBackColor = true;
+            this.BtnStartListing.Click += new System.EventHandler(this.BtnStartListing_Click);
+            // 
+            // cboUsers
+            // 
+            this.cboUsers.FormattingEnabled = true;
+            this.cboUsers.Location = new System.Drawing.Point(471, 12);
+            this.cboUsers.Name = "cboUsers";
+            this.cboUsers.Size = new System.Drawing.Size(156, 20);
+            this.cboUsers.TabIndex = 2;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 39);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(615, 171);
+            this.txtLog.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "输入要发动到客户端的消息";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 228);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(615, 88);
+            this.txtMsg.TabIndex = 3;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 333);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(431, 21);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(449, 322);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(82, 20);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "选择文件";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.AccessibleRole = System.Windows.Forms.AccessibleRole.Equation;
+            this.btnSendFile.Location = new System.Drawing.Point(537, 322);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(82, 20);
+            this.btnSendFile.TabIndex = 1;
+            this.btnSendFile.Text = "发送文件";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(449, 348);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(82, 20);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "发动消息";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnZD
+            // 
+            this.btnZD.Location = new System.Drawing.Point(537, 348);
+            this.btnZD.Name = "btnZD";
+            this.btnZD.Size = new System.Drawing.Size(82, 20);
+            this.btnZD.TabIndex = 1;
+            this.btnZD.Text = "震动";
+            this.btnZD.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 377);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.cboUsers);
+            this.Controls.Add(this.btnZD);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.BtnStartListing);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "Socket聊天服务端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button BtnStartListing;
+        private System.Windows.Forms.ComboBox cboUsers;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnZD;
+    }
+}
+
---
+++ SocketChatServer/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketChatServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStartListing_Click(object sender, EventArgs e)
+        {
+            //创建负责监听的Socket
+            Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //获取要监听的IP地址，把IP字符串转为IP
+            IPAddress ip = IPAddress.Parse(txtIP.Text);
+            //获取要监听的IP和端口，合并为一个变量（创建要监听的IP和端口）
+            IPEndPoint point = new IPEndPoint(ip,Convert.ToInt32(txtPort.Text));
+            //绑定要监听的ip和端口号
+            socketWatch.Bind(point);
+            //给出监听提示
+            ShowMsg("监听成功");
+            //开始监听。设置服务器在一个时间点内最多能够监听的队列数量。
+            socketWatch.Listen(10);
+
+            Thread thread = new Thread(Listen);
+            thread.IsBackground = true;
+            thread.Start(socketWatch);
+
+        }
+
+        void ShowMsg(string str) {
+            //如果不是创建这个控件的线程来调用它
+            if (txtLog.InvokeRequired)
+            {
+                if (txtLog.InvokeRequired) {
+                    //跨线程访问
+                    txtLog.Invoke(new Action<string>(s =>
+                    {
+                        txtLog.AppendText(s + "\r\n");
+                    }), str);
+                }
+            }
+            else {
+                txtLog.AppendText(str+"\r\n");
+            }
+        }
+
+        /// <summary>
+        /// 等待客户端的连接，同时创建一个与之通信的socket
+        /// </summary>
+        void Listen(object obj) {
+            //下面开始等待客户端来连接，同时会创建一个负责通讯的socket
+            Socket socketWatch = obj as Socket;
+            while (true)
+            {
+                Socket socketCommunication = socketWatch.Accept();
+                //给出连接成功提示信息。
+                ShowMsg(socketCommunication.RemoteEndPoint.ToString() + ":连接成功");
+                //开启一个新的线程不停的接收客户端发来的消息
+                Thread thread = new Thread(Receive);
+                thread.IsBackground = true;//最好设置为后台线程，为什么？
+                thread.Start(socketCommunication);
+
+            }
+        }
+
+        void Receive(object obj) {
+            Socket socketCommunication = obj as Socket;
+            //定义一个2M大小的字节数组
+            byte[] buffer = new byte[1024*1024*2];
+            while (true)
+            {
+                try
+                {
+                    //Receive方法的参数表示接收到的数据存储的位置，需要的是一个字节数组，返回值为int
+                    int size = socketCommunication.Receive(buffer);
+                    if (size == 0)
+                    {
+                        //关闭并退出Socket
+                        socketCommunication.Shutdown(SocketShutdown.Both);
+                        socketCommunication.Close();
+                        return;
+                    }
+                    //string str1 = Encoding.Default.GetString(buffer, 0, r);
+                    //通过Encodeing从buffer字节数组中的0个位置开始，把r个字节转成字符串；使用Default编写也可以
+                    string str = Encoding.UTF8.GetString(buffer, 0, size);
+                    //把接收到的数据放到文本框中
+                    ShowMsg(socketCommunication.RemoteEndPoint.ToString() + ":" + str);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+               
+            }
+        }
+    }
+}
